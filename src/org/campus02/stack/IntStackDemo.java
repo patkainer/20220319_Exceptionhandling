752c64a60911da914bbfbcac7079b0d6dc74e743
Patrick Kainer <kainer.patrick@gmail.com>
2022-03-19 11:50:28 +0100

    implemented try catch

--- src/org/campus02/stack/IntStackDemo.java
+++ src/org/campus02/stack/IntStackDemo.java
@@ -14,5 +14,14 @@ public class IntStackDemo {
         System.out.println("push = " + 2);
         stack.push(2);
         System.out.println("stack = " + stack);
+        try{
+            stack.pop();
+            System.out.println(stack);
+            stack.pop();
+
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 }
